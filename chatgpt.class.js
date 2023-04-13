6c115ea9da46d3cf950123cb53f72e59f70ce9ce
dartx <castete>
2023-04-13 12:51:10 -0500

    init con error gpt

---
+++ chatgpt.class.js
@@ -0,0 +1,56 @@
+const { CoreClass } = require('@bot-whatsapp/bot');
+
+require('dotenv').config()
+
+class ChatGPTClass extends CoreClass{
+  queue = []; 
+  optionsGPT = { model: "gpt-3.5-turbo-0301" };
+  openai = undefined;
+
+  constructor( _database, _provider) {
+    super(null, _database, _provider);
+    this.init().then();
+  }
+
+  /**
+   * Esta funciona inicializa
+   */
+  init = async () => {
+    const { ChatGPTAPI } = await import("chatgpt");
+    this.openai = new ChatGPTAPI(
+        {
+            apiKey: process.env.OPENAI_API_KEY
+        }
+    );
+  };
+
+  /**
+   * Manejador de los mensajes
+   * sun funcion es enviar un mensaje a wahtsapp
+   * @param {*} ctx 
+   */
+  handleMsgChatGPT = async (ctx) => {
+    const {from, body} = ctx;
+    const interaccionChatGPT = await this.openai.sendMessage(body, {
+      conversationId: !this.queue.length
+        ? undefined
+        : this.queue[this.queue.length - 1].conversationId,
+      parentMessageId: !this.queue.length
+        ? undefined
+        : this.queue[this.queue.length - 1].id,
+    });
+
+    this.queue.push(interaccionChatGPT);
+
+    const parseMessage = {
+      ...interaccionChatGPT,
+      answer: interaccionChatGPT.text,
+    };
+
+    this.sendFlowSimple([parseMessage], from);
+
+    // return interaccionChatGPT
+  };
+}
+
+module.exports = ChatGPTClass;
